be5fc837ab93c803a99d3a5e3b75bc834de07413
Rabee Aljaramani <rabeejermani@gmail.com>
2022-11-21 10:27:59 +0400

    logos

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 import aldo from './body/logos/aldo.svg';
 import aldo_acc from './body/logos/aldoacc.svg';
 import aero from './body/logos/aero.svg';
-import berkin from './body/logos/berkin.png';
+import birken from './body/logos/birken.svg';
 import ccc from './body/logos/ccc.svg';
 import crocs from './body/logos/crocs.png';
 import dune from './body/logos/dune.png';
-import inglot from './body/logos/inglot.png';
-import lc from './body/logos/lc.png';
+import inglot from './body/logos/inglot.svg';
+import lc from './body/logos/lc.svg';
 import levis from './body/logos/levis.svg';
 import lav from './body/logos/lav.svg';
-import nat from './body/logos/nat.png';
+import nat from './body/logos/nat.svg';
 import rb from './body/logos/rb.svg';
 import skechers from './body/logos/skechers.svg';
-import nine from './body/logos/nine.png';
+import nine from './body/logos/nine.svg';
 import smyk from './body/logos/smyk.png';
 import spring from './body/logos/callitspring.svg';
 import ath from './body/logos/ath.svg';
@@ -29,177 +29,209 @@ export const data_list = [
     id: 1,
     brand: 'Aldo',
     logo: aldo,
-    offer:'Flat 50% off on Selected',
-    stores: ['City Center Muscat','Mall of Oman','Muscat Grand Mall','Oman Avenues Mall','Mall of Muscat'],
+    offer: 'Flat 50% off on Selected',
+    stores: [
+      'City Center Muscat',
+      'Mall of Oman',
+      'Muscat Grand Mall',
+      'Oman Avenues Mall',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 2,
     brand: 'Aldo Acc',
-    offer:'Flat 50% off on Selected',
+    offer: 'Flat 50% off on Selected',
     logo: aldo_acc,
-    stores: ['Mall of Oman','Muscat Grand Mall','Mall of Muscat'],
+    stores: ['Mall of Oman', 'Muscat Grand Mall', 'Mall of Muscat'],
   },
   {
     id: 3,
     brand: 'CCC',
     logo: ccc,
-    offer:'Flat 40% Off on Selected',
+    offer: 'Flat 40% Off on Selected',
     stores: ['Muscat Grand Mall'],
   },
   {
     id: 4,
     brand: 'Call it Spring',
     logo: spring,
-    offer:'Flat 50% off on Selected',
+    offer: 'Flat 50% off on Selected',
     stores: ['City Center Muscat'],
   },
   {
     id: 5,
     brand: 'SMYK',
     logo: smyk,
-    offer:'Sale up to 70% off selected styles',
+    offer: 'Sale up to 70% off selected styles',
     stores: ['Mall of Muscat'],
   },
   {
     id: 6,
     brand: 'LC Waikiki',
     logo: lc,
-    offer:'Flat 30% Off On Everything',
-    stores: ['City Center Muscat','Mall of Oman','Oman Avenues Mall'],
+    offer: 'Flat 30% Off On Everything',
+    stores: ['City Center Muscat', 'Mall of Oman', 'Oman Avenues Mall'],
   },
   {
     id: 7,
     brand: 'R&B',
     logo: rb,
-    offer:'Flat 30% Off On Everything',
-    stores: ['Muscat','Barka','Sohar','Salalah'],
+    offer: 'Flat 30% Off On Everything',
+    stores: ['Muscat', 'Barka', 'Sohar', 'Salalah'],
   },
   {
     id: 8,
     brand: 'Aeropostale',
     logo: aero,
-    offer:'Flat 50% off on everything',
-    stores: ['Muscat Grand Mall','Mall of Oman','Oman Avenues Mall'],
+    offer: 'Flat 50% off on everything',
+    stores: ['Muscat Grand Mall', 'Mall of Oman', 'Oman Avenues Mall'],
   },
-  
+
   {
     id: 10,
     brand: 'CHARLES & KEITH',
     logo: charleskeith,
-    offer:'25 to 40% off on selected',
-    stores: ['City Center Muscat','Mall of Oman','Muscat Grand Mall','Mall of Muscat'],
+    offer: '25 to 40% off on selected',
+    stores: [
+      'City Center Muscat',
+      'Mall of Oman',
+      'Muscat Grand Mall',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 11,
     brand: 'Dune London',
     logo: dune,
-    offer:'Flat 40% off on selected',
-    stores: ['City Center Muscat','Mall of Oman','Muscat Grand Mall','Mall of Muscat'],
+    offer: 'Flat 40% off on selected',
+    stores: [
+      'City Center Muscat',
+      'Mall of Oman',
+      'Muscat Grand Mall',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 12,
     brand: 'NINE WEST',
     logo: nine,
-    offer:'Flat 40% off everything',
+    offer: 'Flat 40% off everything',
     stores: ['Muscat Grand Mall'],
   },
   {
     id: 13,
     brand: 'NATURALISER',
     logo: nat,
-    offer:'Sale 25 - 50% off',
+    offer: 'Sale 25 - 50% off',
     stores: ['Muscat Grand Mall'],
   },
   {
     id: 14,
     brand: 'Levis',
     logo: levis,
-    offer:'Flat 40% off on selected',
-    stores: ['City Center Muscat','Mall of Oman','Mall of Muscat'],
+    offer: 'Flat 40% off on selected',
+    stores: ['City Center Muscat', 'Mall of Oman', 'Mall of Muscat'],
   },
   {
     id: 15,
     brand: 'Beverly Hills & Polo Club',
     logo: beverly,
-    offer:'Flat 40% on everything',
-    stores: ['City Center Muscat','Mall of Oman','Mall of Muscat'],
+    offer: 'Flat 40% on everything',
+    stores: ['City Center Muscat', 'Mall of Oman', 'Mall of Muscat'],
   },
   {
     id: 16,
     brand: 'Ardene',
     logo: ardene,
-    offer:'Flat 50% on Everything',
-    stores: ['City Center Muscat','Mall of Oman'],
+    offer: 'Flat 50% on Everything',
+    stores: ['City Center Muscat', 'Mall of Oman'],
   },
   {
     id: 17,
     brand: 'Skechers',
     logo: skechers,
-    offer:'Flat 40% on selected',
-    stores: ['City Center Muscat','Mall of Oman','Muscat Grand Mall','Oman Avenues Mall','Mall of Muscat'],
+    offer: 'Flat 40% on selected',
+    stores: [
+      'City Center Muscat',
+      'Mall of Oman',
+      'Muscat Grand Mall',
+      'Oman Avenues Mall',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 18,
     brand: 'CROCS',
     logo: crocs,
-    offer:'25% to 50% on Selected Styles',
-    stores: ['City Center Muscat','Mall of Oman','Muscat Grand Mall','Oman Avenues Mall','Mall of Muscat'],
+    offer: '25% to 50% on Selected Styles',
+    stores: [
+      'City Center Muscat',
+      'Mall of Oman',
+      'Muscat Grand Mall',
+      'Oman Avenues Mall',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 19,
     brand: 'ADIDAS',
     logo: adidas,
-    offer:'25% to 50% sale on selected',
-    stores: ['City Center Muscat','Mall of Oman'],
+    offer: '25% to 50% sale on selected',
+    stores: ['City Center Muscat', 'Mall of Oman'],
   },
   {
     id: 20,
     brand: 'Atheletes co.',
     logo: ath,
-    offer:'25% to 50% sale on selected',
+    offer: '25% to 50% sale on selected',
     stores: ['City Center Qurum'],
   },
   {
     id: 21,
     brand: 'Birkenstock',
-    logo: berkin,
-    offer:'25% to 50% on Selected Styles',
-    stores: ['Muscat Grand Mall','Oman Avenues Mall','Mall of Oman'],
+    logo: birken,
+    offer: '25% to 50% on Selected Styles',
+    stores: ['Muscat Grand Mall', 'Oman Avenues Mall', 'Mall of Oman'],
   },
   {
     id: 22,
     brand: 'Rituals',
     logo: rituals,
-    offer:'25% on selected items',
-    stores: ['Muscat Grand Mall','City Center Muscat','Mall of Oman'],
+    offer: '25% on selected items',
+    stores: ['Muscat Grand Mall', 'City Center Muscat', 'Mall of Oman'],
   },
   {
     id: 23,
     brand: 'Inglot',
     logo: inglot,
-    offer:'Buy 2 Get 2 free on Everything',
-    stores: ['Muscat Grand Mall','City Center Muscat','Mall of Oman','Mall of Muscat'],
+    offer: 'Buy 2 Get 2 free on Everything',
+    stores: [
+      'Muscat Grand Mall',
+      'City Center Muscat',
+      'Mall of Oman',
+      'Mall of Muscat',
+    ],
   },
   {
     id: 24,
     brand: 'La vie n rose',
     logo: lav,
-    offer:'Flat 50% on Everything',
-    stores: ['Mall of Oman','Muscat Grand Mall'],
+    offer: 'Flat 50% on Everything',
+    stores: ['Mall of Oman', 'Muscat Grand Mall'],
   },
   {
     id: 25,
     brand: 'New Yorker',
     logo: newyorker,
-    offer:'Flat 25% off on selected',
+    offer: 'Flat 25% off on selected',
     stores: ['Mall of Oman'],
   },
   {
     id: 26,
     brand: 'R&B Kids',
     logo: rbkids,
-    offer:'Flat 40% off selected',
+    offer: 'Flat 40% off selected',
     stores: ['Mall of Oman'],
   },
-
 ];

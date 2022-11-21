01a7f2b9f2011947ba3998ec16cb12683229eed6
Rabee Aljaramani <rabeejermani@gmail.com>
2022-11-21 12:15:20 +0400

    fin

--- src/body/Body.js
+++ src/body/Body.js
@@ -6,14 +6,14 @@ export default function Body() {
   useEffect(() => {}, []);
   return (
     <div className="container">
-      <div className="title">{/* <h2>CA HALF BACK LIST</h2> */}</div>
       <div className="data-grid-container center-it">
         {data_list.map((item) => (
           <Card
             key={item.id}
             brand={item.brand}
             logo={item.logo}
-            offer={item.offer}
+            offer1={item.offer1}
+            offer2={item.offer2}
             stores={item.stores}
           />
         ))}
--- src/body/Card.js
+++ src/body/Card.js
@@ -1,28 +1,31 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
 import React, { useState } from 'react';
-
+import tag from './logos/tag.svg';
 export default function Card(props) {
   // eslint-disable-next-line no-unused-vars
   const [value, setValue] = useState('');
   return (
     <div className="card container">
-      <img src={props.logo} alt="logo" />
-      <div className="sale">{props.offer}</div>
-
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Stores</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={value}
-          label="Stores"
-        >
-          {props.stores.map((store) => (
-            <MenuItem value={store}>{store}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <div className="terms">*T&C Apply</div>
+      <img className="tag" src={tag} alt="logo" />
+      <img className="logo" src={props.logo} alt="logo" />
+      <div className="sale1">{props.offer1}</div>
+      <div className="sale2">{props.offer2}</div>
+      <div className="form">
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Stores</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={value}
+            label="Stores"
+          >
+            {props.stores.map((store) => (
+              <MenuItem value={store}>{store}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <div className="terms">*T&C Apply</div>
+      </div>
     </div>
   );
 }
